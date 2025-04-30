1d1f82aa8d239934a8c568c42fb68ad95604d94b
iVinci-koudaiKAWANISHI <kodai.kawanishi@i-vinci.co.jp>
2025-04-30 17:21:59 +0900

    細かい処理の修正・共通化

--- todo_apiproject/todo_apiapp/models.py
+++ todo_apiproject/todo_apiapp/models.py
@@ -3,6 +3,5 @@ from django.db import models
 # Create your models here.
 class Todo(models.Model):
     task = models.CharField(max_length=100)
-    due_date = models.DateTimeField(null=True,blank=True)
+    due_date = models.DateField(null=True,blank=True)
     completed = models.BooleanField(default=False)
-    
--- todo_apiproject/todo_apiapp/views.py
+++ todo_apiproject/todo_apiapp/views.py
@@ -3,20 +3,25 @@ from .models import Todo
 from django.views.decorators.csrf import csrf_exempt
 import json
 
+
+def create_response(todo, status):
+    response = JsonResponse({
+        'id':todo.id,
+        'task':todo.task,
+        'due_date':todo.due_date,
+        'completed':todo.completed,
+    }, status=status)
+    return response
+
 @csrf_exempt
 def create_todo(request):
     if request.method == 'POST':
         data = json.loads(request.body)
         task = data.get('task')
         due_date = data.get('due_date')
-        completed = data.get('completed')
+        completed = data.get('completed', False)
         todo = Todo.objects.create(task=task,due_date=due_date,completed=completed)
-        response = JsonResponse({
-            'id':todo.id,
-            'task':todo.task,
-            'due_date':todo.due_date,
-            'completed':todo.completed,
-        },status=201)
+        response = create_response(todo, 201)
         return response
 
 @csrf_exempt
@@ -31,12 +36,7 @@ def get_all_todos(request):
 def get_todo(request, id):
     if request.method == 'GET':
         todo = Todo.objects.get(id=id)
-        response = JsonResponse({
-            'id':todo.id,
-            'task':todo.task,
-            'due_date':todo.due_date,
-            'completed':todo.completed,
-        },status=200)
+        response = create_response(todo, 200)
         return response
 
 @csrf_exempt
@@ -46,14 +46,9 @@ def update_todo(request, id):
         data = json.loads(request.body)
         todo.task = data.get('task')
         todo.due_date = data.get('due_date')
-        todo.completed = data.get('completed')
+        todo.completed = data.get('completed', False)
         todo.save()
-        response = JsonResponse({
-            'id':todo.id,
-            'task':todo.task,
-            'due_date':todo.due_date,
-            'completed':todo.completed,
-        },status=200)
+        response = create_response(todo, 200)
         return response
 
 @csrf_exempt
@@ -62,6 +57,10 @@ def delete_todo(request, id):
         todo = Todo.objects.get(id=id)
         task = todo.task
         todo.delete()
-        response = JsonResponse({'message':f'id:{id} task:{task} を削除しました。'},status=200, json_dumps_params={'ensure_ascii': False})
+        response = JsonResponse(
+            {'message':f'id:{id} task:{task} を削除しました。'},
+            status=200,
+            json_dumps_params={'ensure_ascii': False}
+        )
         return response
     
