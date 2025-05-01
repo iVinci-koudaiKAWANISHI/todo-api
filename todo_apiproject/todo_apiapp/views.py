14a1d1171ab396ca949d3a808a3b31499826f20a
iVinci-koudaiKAWANISHI <kodai.kawanishi@i-vinci.co.jp>
2025-05-01 18:01:55 +0900

    GETメソッド処理実装

--- todo_apiproject/todo_apiapp/views.py
+++ todo_apiproject/todo_apiapp/views.py
@@ -13,59 +13,22 @@ def create_response(todo, status):
     }, status=status)
     return response
 
-# 新しいTodoタスクを作成するAPIエンドポイント
-@csrf_exempt
-def create_todo(request):
-    if request.method == 'POST':
-        data = json.loads(request.body)
-        task = data.get('task')
-        due_date = data.get('due_date')
-        completed = data.get('completed', False)
-        todo = Todo.objects.create(task=task,due_date=due_date,completed=completed)
-        response = create_response(todo, 201)
-        return response
-
-# すべてのTodoタスクを取得する
-@csrf_exempt
-def get_all_todos(request):
-    if request.method == 'GET':
-        todos = Todo.objects.all()
-        todos_list = list(todos.values())
-        response = JsonResponse(todos_list, safe=False, status=200)
-        return response
-
 # 特定のIDのTodoタスクを取得するAPIエンドポイント
 @csrf_exempt
-def get_todo(request, id):
+def todo_detail(request, id):
     if request.method == 'GET':
-        todo = Todo.objects.get(id=id)
+        try:
+            todo = Todo.objects.get(id=id)
+        except Todo.DoesNotExist:
+            return JsonResponse({'detail':'Not found'}, status=404)
         response = create_response(todo, 200)
         return response
-
-# Todoタスクを更新するエンドポイント
-@csrf_exempt
-def update_todo(request, id):
-    if request.method == 'PUT':
-        todo = Todo.objects.get(id=id)
+    elif request.method == 'PUT':
+        try:
+            todo = Todo.objects.get(id=id)
+        except Todo.DoesNotExist:
+            return JsonResponse({'detail':'Not found'}, status=404)
         data = json.loads(request.body)
         todo.task = data.get('task')
         todo.due_date = data.get('due_date')
-        todo.completed = data.get('completed', False)
-        todo.save()
-        response = create_response(todo, 200)
-        return response
-
-# Todoタスクを削除するAPIエンドポイント
-@csrf_exempt
-def delete_todo(request, id):
-    if request.method == 'DELETE':
-        todo = Todo.objects.get(id=id)
-        task = todo.task
-        todo.delete()
-        response = JsonResponse(
-            {'message':f'id:{id} task:{task} を削除しました。'},
-            status=200,
-            json_dumps_params={'ensure_ascii': False}
-        )
-        return response
-    
+        todo.completed = data.get('completed')
