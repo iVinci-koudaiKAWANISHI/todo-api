00e4099d6eda74f82f66406159abdec19f9edac9
iVinci-koudaiKAWANISHI <kodai.kawanishi@i-vinci.co.jp>
2025-05-02 18:27:25 +0900

    テストコード作成

--- todo_apiproject/todo_apiapp/views.py
+++ todo_apiproject/todo_apiapp/views.py
@@ -1,10 +1,9 @@
 from django.http import JsonResponse
 from .models import Todo
-from django.views.decorators.csrf import csrf_exempt
 import json
 
 # レスポンス作成を共通化
-def create_response(todo, status):
+def todo_response(todo, status):
     response = JsonResponse({
         'id':todo.id,
         'task':todo.task,
@@ -14,21 +13,27 @@ def create_response(todo, status):
     return response
 
 # 特定のIDのTodoタスクを取得するAPIエンドポイント
-@csrf_exempt
 def todo_detail(request, id):
     if request.method == 'GET':
         try:
             todo = Todo.objects.get(id=id)
         except Todo.DoesNotExist:
             return JsonResponse({'detail':'Not found'}, status=404)
-        response = create_response(todo, 200)
+        response = todo_response(todo, 200)
         return response
-    elif request.method == 'PUT':
-        try:
-            todo = Todo.objects.get(id=id)
-        except Todo.DoesNotExist:
-            return JsonResponse({'detail':'Not found'}, status=404)
+    else:
+        return JsonResponse({'detail':'Method Not Allowed'}, status=405)
+
+def todos(request):
+    if request.method == 'GET':
+        todos = Todo.objects.all()
+        todo_list = list(todos.values())
+        response = JsonResponse(todo_list, safe=False, status=200)
+        return response
+    elif request.method == 'POST':
         data = json.loads(request.body)
-        todo.task = data.get('task')
-        todo.due_date = data.get('due_date')
-        todo.completed = data.get('completed')
+        task = data.get('task')
+        due_date = data.get('due_date')
+        todo = Todo.objects.create(task=task, due_date=due_date, completed=False)
+        response = todo_response(todo, 201)
+        return response
